4cd7f3dc1774023d9519798d8266ff621f7ca7bf
Severin Mairinger <mai.sevi@hotmail.com>
2018-04-16 09:29:39 +0200

    Added 95_Recursive

--- UE52_Fibonacci/src/com/maisev/recursive/Dir_Methods.java
+++ UE52_Fibonacci/src/com/maisev/recursive/Dir_Methods.java
@@ -1,4 +1,36 @@
 package com.maisev.recursive;
 
+import java.io.File;
+import java.util.ArrayList;
+
 public class Dir_Methods {
+    private ArrayList<String> directories = new ArrayList<>();
+    int layer = 0;
+
+    public ArrayList<String> getDirectories(File root) {
+        File[] list = root.listFiles();
+        StringBuilder tabs = new StringBuilder();
+
+        if (list == null) return null;
+
+        for ( File f : list ) {
+            if ( f.isDirectory() ) {
+                layer++;
+                for (int i = 0; i < layer; i++) {
+                    tabs.append("-");
+                }
+                getDirectories(new File(f.getAbsolutePath()));
+                directories.add(tabs + " Dir:  " + f.getAbsoluteFile() );
+            }
+            else {
+                for (int i = 0; i < layer; i++) {
+                    tabs.append("-");
+                }
+                directories.add(tabs + ">File:  " + f.getAbsoluteFile() );
+            }
+        }
+
+        layer--;
+        return directories;
+    }
 }
--- UE52_Fibonacci/src/com/maisev/recursive/Main.java
+++ UE52_Fibonacci/src/com/maisev/recursive/Main.java
@@ -1,5 +1,7 @@
 package com.maisev.recursive;
 
+import java.io.File;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
@@ -7,7 +9,11 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         UE2_Methods ue2 = new UE2_Methods();
+        Dir_Methods ue3 = new Dir_Methods();
+        ArrayList<String> arrayList;
         int n = 0;
+        String directory;
+        File nameDir;
     // UE2
         System.out.print("\n\t n: ");
         n = sc.nextInt();
@@ -16,5 +22,21 @@ public class Main {
         System.out.println(ue2.fib_iterative(n));
         System.out.println(ue2.fib_recursive(n));
     // Directory
+        while (true) {
+            System.out.print("\n\nPlease enter a Directory Path: ");
+            directory = sc.next();
+
+            try {
+                nameDir = new File(directory);
+                break;
+            } catch (NullPointerException e) {
+                System.out.println("\n\t File not found! Possibly a wrong path?");
+            }
+        }
+        arrayList = ue3.getDirectories(nameDir);
+
+        for (String p:arrayList) {
+            System.out.println(p);
+        }
     }
 }
